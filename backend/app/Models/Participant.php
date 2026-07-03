fa4e8ac8dd0d6286c27bcce28109310ae0ac2b73
RiskaVanya <annastasyarv06_@gmail.com>
2026-07-03 18:35:04 +0700

    Feat: melakukan pembaruan pada model dan migrasi backend

--- backend/app/Models/Participant.php
+++ backend/app/Models/Participant.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'quiz_id',
-        'name'
+        'name',
+        'score'
     ];
 
     public function quiz()
--- backend/database/migrations/2026_06_21_052942_create_participants_table.php
+++ backend/database/migrations/2026_06_21_052942_create_participants_table.php
@@ -6,22 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-   public function up(): void
-{
-    Schema::create('participants', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('quiz_id')->constrained()->onDelete('cascade');
-        $table->string('name');
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('quiz_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('score')->default(0);
+            $table->timestamps();
+        });
+    }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('participants');
